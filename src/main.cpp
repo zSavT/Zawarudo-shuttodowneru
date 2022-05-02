d43bbaccb2ad052fe38f63750d7ea491eef3c361
zSavT <verioc3@gmail.com>
2022-05-02 15:21:53 +0200

    add src file

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<windows.h>
+
+using namespace std;
+
+int main()
+{
+    system("start https://www.youtube.com/watch?v=7ePWNmLP0Z0");
+    Sleep(7500);
+    system("start explorer.exe");
+    return 0;
+}
